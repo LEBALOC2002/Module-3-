6b46e483564a2da6e507569346e1d70896bfb4cb
LEBALOC2002 <108645216+LEBALOC2002@users.noreply.github.com>
2022-10-06 11:53:33 +0700

    hiển thị dòng thời gian hiện tại

---
+++ CSS_module3/tong-quan-ung-dung-wed/demo/src/main/java/com/example/demo/HelloServlet.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.Date;
+
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        Date today = new Date();
+
+        writer.println("<h1>" + today + "</h1>");
+        writer.println("</html>");
+//        // Hello
+//        PrintWriter out = response.getWriter();
+//        out.println("<html><body>");
+//        out.println("<h1>" + message + "</h1>");
+//        out.println("</body></html>");
+    }
+    
+
+    public void destroy() {
+    }
+}
